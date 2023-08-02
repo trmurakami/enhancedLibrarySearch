1b6612a7620b68ff9570d84087797c5cf4ed2ee1
Tiago Murakami <trmurakami@gmail.com>
2023-08-02 15:53:42 -0300

    Keras

--- py/keras.py
+++ py/keras.py
@@ -1,13 +1,11 @@
-from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Embedding, LSTM, Dense
 
-data = pd.read_csv('/var/www/html/ml/py/Scopus_SDG.tsv', delimiter='\t')
+
+data = pd.read_csv('/var/www/html/ml/py/data/Scopus_SDG.tsv', delimiter='\t')
 texts = data['Data'].tolist()
 labels = data['Label'].tolist()
 
@@ -16,18 +14,18 @@ texts, labels = shuffle(texts, labels, random_state=42)
 # Tokenização dos textos
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(texts)
-sequences = tokenizer.texts_to_sequences(texts)
+sequences = text_to_sequences(texts)
 
-# Tamanho máximo das sequências (ajuste conforme necessário)
+# # Tamanho máximo das sequências (ajuste conforme necessário)
 max_sequence_length = 128
 
-# Padding das sequências para o mesmo tamanho
+# # Padding das sequências para o mesmo tamanho
 sequences_padded = pad_sequences(sequences, maxlen=max_sequence_length)
 
-# Número total de palavras no vocabulário
+# # Número total de palavras no vocabulário
 vocab_size = len(tokenizer.word_index) + 1
 
-# Converter as etiquetas em números (opcional, dependendo do formato das etiquetas)
+# # Converter as etiquetas em números (opcional, dependendo do formato das etiquetas)
 
 label_encoder = LabelEncoder()
 encoded_labels = label_encoder.fit_transform(labels)
@@ -36,9 +34,9 @@ num_classes = len(label_encoder.classes_)
 X_train, X_test, y_train, y_test = train_test_split(
     sequences_padded, encoded_labels, test_size=0.2, random_state=42)
 
-model = Sequential()
+model = keras.Sequential()
 model.add(Embedding(vocab_size, 10,
-          input_length=max_sequence_length, mask_zero=True))
+                    input_length=max_sequence_length, mask_zero=True))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
@@ -52,10 +50,10 @@ print(f"Loss: {loss:.4f}, Accuracy: {accuracy:.4f}")
 
 model.save('modelo_text_classifier.keras')
 
-predictions = model.predict(X_test)
-y_pred = predictions.argmax(axis=1)
-y_test = y_test.astype('int')
-print(y_pred)
-print(y_test)
-print(label_encoder.inverse_transform(y_pred))
-print(label_encoder.inverse_transform(y_test))
+# predictions = model.predict(X_test)
+# y_pred = predictions.argmax(axis=1)
+# y_test = y_test.astype('int')
+# print(y_pred)
+# print(y_test)
+# print(label_encoder.inverse_transform(y_pred))
+# print(label_encoder.inverse_transform(y_test))
