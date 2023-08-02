95a53ad6e49bc34eee80f696c7c65947c762484d
Tiago Murakami <trmurakami@gmail.com>
2023-08-02 20:19:19 -0300

    ML

--- py/keras_convert_modeljs.py
+++
@@ -1,6 +0,0 @@
-import tensorflowjs as tfjs
-import tensorflow as tf
-from keras.models import load_model
-
-model = load_model('modelo_text_classifier.keras')
-tfjs.converters.save_keras_model(model, 'Model_js')
