a472e0cf1d5394e016e6699804f0017134e51e4a
Tiago Murakami <trmurakami@gmail.com>
2023-08-08 00:26:44 -0300

    ML

--- index.js
+++ index.js
@@ -45,13 +45,13 @@ function tokenize(text) {
     // create a list of slices of the list of tokens
     let i = 0;
     tokenized_text_segments = [];
-    while (i+100 < Math.max(tokens.length, 304)) {
-        var new_slice = tokens.slice(i,i+304);
-        while (new_slice.length < 304) {
+    while (i+100 < Math.max(tokens.length, 400)) {
+        var new_slice = tokens.slice(i,i+400);
+        while (new_slice.length < 400) {
             new_slice.push(0);
           }
         tokenized_text_segments.push(new_slice);
-        i = i + 50;
+        i = i + 100;
     }
     return tokenized_text_segments;
   }
@@ -63,7 +63,7 @@ async function predictParty() {
         var stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"];
         text = text.replace(stopwords, "")
         var x = tokenize(text)
-        x = model.predict(tf.tensor2d(x, [x.length, 304]));
+        x = model.predict(tf.tensor2d(x, [x.length, 400]));
 
         x.mean(0).print();
         console.log(x);
